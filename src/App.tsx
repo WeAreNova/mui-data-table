40eb4edd27ef5e87b61c0bf698b306930537ce60
David <david.parrish@wearenova.co.uk>
2021-10-23 17:25:44 +0100

    Changes for improving developer experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import MomentUtils from "@date-io/moment";
-import { CssBaseline, TableContainer } from "@material-ui/core";
+import { Box, createTheme, CssBaseline, IconButton, TableContainer, ThemeProvider } from "@material-ui/core";
+import Brightness4 from "@material-ui/icons/Brightness4";
+import Brightness7 from "@material-ui/icons/Brightness7";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Table from "./lib";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(window.localStorage.getItem("darkMode") === "true");
+  const toggleDarkMode = useCallback(() => setDarkMode((currDarkMode) => !currDarkMode), []);
+  useEffect(() => {
+    window.localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
+
   return (
-    <MuiPickersUtilsProvider utils={MomentUtils}>
+    <ThemeProvider theme={createTheme({ palette: { type: darkMode ? "dark" : "light" } })}>
       <CssBaseline />
-      <TableContainer>
-        <Table tableData={[]} tableStructure={[]} />
-      </TableContainer>
-    </MuiPickersUtilsProvider>
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <Box display="flex" justifyContent="flex-end" marginBottom={2} paddingRight={1} paddingTop={1}>
+          <IconButton onClick={toggleDarkMode}>{darkMode ? <Brightness7 /> : <Brightness4 />}</IconButton>
+        </Box>
+        <TableContainer>
+          <Table tableData={[]} tableStructure={[]} />
+        </TableContainer>
+      </MuiPickersUtilsProvider>
+    </ThemeProvider>
   );
 }
 

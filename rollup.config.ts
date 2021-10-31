a9c6de351fef05b51de2797a196835625f48c002
David <david.parrish@wearenova.co.uk>
2021-10-31 14:49:48 +0000

    Improvements for development, build and `peerDependencies`

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,13 +3,21 @@ import typeScriptPlugin from "@rollup/plugin-typescript";
 import path from "path";
 import { defineConfig } from "rollup";
 import copyPlugin from "rollup-plugin-copy";
-import peerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
 import progressPlugin from "rollup-plugin-progress";
 import packageJSON from "./lib/package.json";
 
+const EXTERNALS = Object.keys({
+  ...packageJSON.dependencies,
+  ...packageJSON.peerDependencies,
+}).concat(["clsx"]);
+
+const EXTERNALS_REGEX = new RegExp(EXTERNALS.join("|"));
+
+const isExternal = (id) => EXTERNALS_REGEX.test(id);
+
 export default defineConfig({
   input: "lib/src/index.tsx",
-  external: ["dot-prop", "js-file-download", "natural-orderby", "clsx"],
+  external: isExternal,
   output: [
     {
       file: path.join("build", packageJSON.module),
@@ -24,7 +32,6 @@ export default defineConfig({
     },
   ],
   plugins: [
-    peerDepsExternalPlugin({ packageJsonPath: "./lib/package.json" }),
     progressPlugin(),
     commonjsPlugin(),
     typeScriptPlugin({ tsconfig: "lib/tsconfig.json" }),
